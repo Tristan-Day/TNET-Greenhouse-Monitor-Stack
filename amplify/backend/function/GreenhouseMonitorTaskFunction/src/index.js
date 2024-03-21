d44e4792b361ce82a265c0f410e4eb538d75b441
Tristan Day <tristan.day.uk@gmail.com>
2024-03-21 18:55:35 +0000

    Fix missing parameter for timestamp task

--- amplify/backend/function/GreenhouseMonitorTaskFunction/src/index.js
+++ amplify/backend/function/GreenhouseMonitorTaskFunction/src/index.js
@@ -2,6 +2,9 @@
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 
+const CHRONOS_HOUR = 1000 * 60 * 60
+const CHRONOS_DAY = CHRONOS_HOUR * 24
+
 exports.handler = async event => {
   console.log(`EVENT: ${JSON.stringify(event)}`)
 
@@ -9,7 +12,7 @@ exports.handler = async event => {
   const updateTimestamps = require('./tasks/updateTimestamps')
 
   try {
-    await Promise.all([pruneRecords(), updateTimestamps()])
+    await Promise.all([pruneRecords(), updateTimestamps(CHRONOS_HOUR * 2)])
     console.log('Exectuion Complete')
   } catch (error) {
     console.log(`Failed to Complete Tasks: ${error}`)
